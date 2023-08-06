2ce773d743e9f7fa3008d945869d025447976d43
sam <sampurna.chapagainnn@gmail.com>
2023-08-06 13:58:08 +0100

    add url for login and register

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,8 @@ from . import views
 from django.conf import settings
 
 from django.conf.urls.static import static
+from .forms import LoginForm
+from django.contrib.auth import views as auth_views
 
 app_name='store'
 
@@ -11,6 +13,9 @@ urlpatterns = [
     path('', views.products, name='products'),
     path('checkout/', views.checkout, name='checkout'),
     path('product/<int:pk>/', views.product_detail, name='product-detail'),
+
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='store/login.html', authentication_form=LoginForm), name='login'),
 ]
 
 if settings.DEBUG:
