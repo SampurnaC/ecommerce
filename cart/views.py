6d2ac4662a6ef6045982953965f10ef75953d379
sam <sampurna.chapagainnn@gmail.com>
2023-08-06 07:22:24 +0100

    views ofr cart to display all orders

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from store.models import *
+from django.db.models import Count
 
-# Create your views here.
+def cart(request):
+    customer = request.user.customer
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    items = order.order_items.all().order_by('-date_added')
+    # items = items.last()
+    # items = order.orderitem_set.all()
+    # breakpoint()
+    # for it in items:
+    #     print(it.product.name)
+    # dups = order.order_items.values(product.name).annotate(Count('id')).order_by('-date_added').filter(id__count__gt=1)
+
+    context = {'items':items, 'order':order}
+    return render(request, 'cart/cart.html', context)
